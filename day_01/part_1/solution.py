a44efa295bab85d465c3d7a9edba9a3bcb8a3163
Emil Merle <emilmerle8@gmail.com>
2023-12-01 20:10:29 +0100

    Added solution with string concatenation for day 01

--- day_01/part_1/solution.py
+++ day_01/part_1/solution.py
@@ -11,6 +11,8 @@ def main():
             line_sum = 0
             digits = re.findall("[0-9]", line)
             if len(digits) >= 2:
+                # String concatenation also possible for two-digit-numbers but cast to int necessary later anyway:
+                # line_sum = int( digits[0] + digits[-1] )
                 line_sum = int(digits[0]) * 10 + int(digits[-1])
             else:
                 line_sum = int(digits[0]) * 11
--- day_01/part_2/solution.py
+++ day_01/part_2/solution.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     # Tricky pattern: We also need to find overlapping cases:
-    # for example: threeight should be 38
-    # The ?=... in the pattern assures this
+    # for example: in "threeight" we should find "three" AND "eight"
+    # The ?=... in the pattern assures overlapping words are found
     regex_pattern = "(?=(one|two|three|four|five|six|seven|eight|nine|[1-9]))"
     filename = "./input_data"
 
@@ -20,6 +20,9 @@ def main():
             if len(matches) == 0:
                 sys.exit(f"No match in line {counter}")
             if len(matches) >= 2:
+                # String concatenation also possible for two-digit-numbers but cast to int necessary later anyway:
+                # line_sum = int(wordToDigitStr(matches[0]) + wordToDigitStr(matches[-1]))
+
                 first_digit = strToInt(matches[0])
                 second_digit = strToInt(matches[-1])
                 line_sum = first_digit * 10 + second_digit
@@ -61,8 +64,34 @@ def wordToInt(digit: str) -> int:
         case "nine":
             return 9
         case other:
-            print("OTHER FOUND")
-            return 0
+            sys.exit("Unknown case found while converting words to digits.")
+
+
+def wordToDigitStr(word: str) -> str:
+    if word.isnumeric():
+        return word
+    else:
+        match word:
+            case "one":
+                return "1"
+            case "two":
+                return "2"
+            case "three":
+                return "3"
+            case "four":
+                return "4"
+            case "five":
+                return "5"
+            case "six":
+                return "6"
+            case "seven":
+                return "7"
+            case "eight":
+                return "8"
+            case "nine":
+                return "9"
+            case other:
+                sys.exit("Unknown case found while converting words to digits.")
 
 
 if __name__ == "__main__":
