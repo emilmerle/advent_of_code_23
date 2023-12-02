e55963ddee5514d692c4c89d7d600c251b9cf115
Emil Merle <emilmerle8@gmail.com>
2023-12-02 12:06:15 +0100

    formatted day 2 solutions with black

--- day_02/part_1/solution.py
+++ day_02/part_1/solution.py
@@ -2,16 +2,13 @@ import re
 
 # Configuration:
 # 12 red cubes, 13 green cubes, 14 blue cubes
-MAX_CUBES_DICT = {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-}
+MAX_CUBES_DICT = {"red": 12, "green": 13, "blue": 14}
 
 game_id_regex_pattern = "(Game [0-9]+)"
 sets_regex_pattern = r"(?=[;:]+ (.+?)[;\n])"
 
 filename = "./input_data"
+
 
 def main():
     with open(filename, "r") as f:
@@ -20,7 +17,9 @@ def main():
             line_game_id = get_game_id(line)
             sets = find_sets(line)
             possible_game = is_game_possible(sets)
-            print(f"Game {line_game_id} is {'possible!' if possible_game else 'not possible!'}")
+            print(
+                f"Game {line_game_id} is {'possible!' if possible_game else 'not possible!'}"
+            )
 
             if possible_game:
                 sum += line_game_id
@@ -32,11 +31,14 @@ def main():
 def get_game_id(line: str) -> int:
     return int(re.search(game_id_regex_pattern, line)[0][5:])
 
+
 def find_sets(line: str) -> str:
     return re.findall(sets_regex_pattern, line)
-      
+
+
 def strToList(cubes: str) -> list:
     return [x.strip() for x in cubes.split(",")]
+
 
 def is_game_possible(sets: list) -> bool:
     possible_game = True
@@ -48,6 +50,7 @@ def is_game_possible(sets: list) -> bool:
             if cubes_dict[cube] > MAX_CUBES_DICT[cube]:
                 possible_game = False
     return possible_game
-        
+
+
 if __name__ == "__main__":
     main()
--- day_02/part_2/solution.py
+++ day_02/part_2/solution.py
@@ -6,6 +6,7 @@ sets_regex_pattern = r"(?=[;:]+ (.+?)[;\n])"
 
 filename = "./input_data"
 
+
 def main():
     with open(filename, "r") as f:
         sum = 0
@@ -21,21 +22,20 @@ def main():
     print(f"The total sum of the power of the sets is {sum}.")
     return
 
+
 def find_sets(line: str) -> list:
     return re.findall(sets_regex_pattern, line)
+
 
 def strToList(cubes: str) -> list:
     return [x.strip() for x in cubes.split(",")]
 
+
 def max_cubes_for_set(sets: str) -> dict:
-    max_cubes_dict_for_set = {
-        "red": 0,
-        "green": 0,
-        "blue": 0
-    }
+    max_cubes_dict_for_set = {"red": 0, "green": 0, "blue": 0}
     for cubes_string in sets:
         cubes_list = strToList(cubes_string)
-        # turn every set into a dict with dict comprehension 
+        # turn every set into a dict with dict comprehension
         cubes_dict = {x.split()[1]: int(x.split()[0]) for x in cubes_list}
         # write highest number of every color into the max_cubes_dict_for_set dict:
         # this equals to the minimum number of cubes needed for this game to be possible
@@ -43,6 +43,7 @@ def max_cubes_for_set(sets: str) -> dict:
             if cubes_dict[cube] > max_cubes_dict_for_set[cube]:
                 max_cubes_dict_for_set[cube] = cubes_dict[cube]
     return max_cubes_dict_for_set
+
 
 if __name__ == "__main__":
     main()
