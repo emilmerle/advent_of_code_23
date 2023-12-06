2ada75933a9505a20ee54a1a1eb5711400b809cd
Emil Merle <emilmerle8@gmail.com>
2023-12-06 10:48:12 +0100

    Added broken solution for day 05 part 2

--- day_05/part_2/solution.py
+++ day_05/part_2/solution.py
@@ -39,23 +39,37 @@ def main():
                 continue
             else:
                 exit("Edge Case found!!!")
+        
         # print(map_dict)
-        # print(combine_two_intervals_and_shift((5,15), (10,20), 7))
-        for seed in seed_ranges:
-            print(seed)
-            current_values = [seed]
-            for map in map_order:
-                print(f"MAP {map} STARTING")
-                next_values = []
-                print(current_values)
-                for tuple in current_values:
-                    for map_value in map_dict[map]:
-                        all_tuples = combine_two_intervals_and_shift(tuple, map_value, map_dict[map][map_value])
-                        next_values += [x for x in all_tuples if x not in next_values]
-                        # print(next_values)
+        
+        current_values = seed_ranges
+        for map in map_order:
+            print(f"MAP {map} STARTING")
+            next_values = []
+            print("Starting with:", current_values)
+            print("Map values:", map_dict[map])
+            for tuple in current_values:
+                tuple_altered = False
+                # Mistake found: If one interval has to be mapped multiple times, too many intervals are returned
+                # TODO: fix
+                for map_value in map_dict[map]:
+                    all_tuples = combine_two_intervals_and_shift(tuple, map_value, map_dict[map][map_value])
+                    if all_tuples != [tuple]:
+                        # print(tuple, all_tuples)
+                        tuple_altered = True
+                    next_values += [x for x in all_tuples if x not in next_values]
+                # print("Before:", next_values)
+                # Mistake found: Remove original interval if the interval was combined at some time
+                if tuple in next_values and tuple_altered:
+                    next_values.pop(next_values.index(tuple))
+                # print("After:", next_values)
+
                 current_values = next_values
 
-        #print(current_values)
+        lowest_location = min([min(x) for x in current_values])
+        print(f"The lowest location number is {lowest_location}.")
+
+        # test_combine()
         
         return 
 
@@ -63,6 +77,31 @@ def main():
 
 
         # print(f"The lowest location number is {min(final_values)}.")
+
+def test_combine():
+    x = 3
+    test_intervals = [
+        ((5,24), (25,30), x), # (5,24)
+        ((11,30), (5,10), x), # (11,30)
+
+        ((5,15), (10,20), x), # (5,9) (13,18)
+        ((5,15), (15,20), x), # (5,14) (18,18)
+
+        ((10, 20), (5,15), x), # (13,18), (16,20)
+        ((15, 20), (10,15), x), # (18,18) (16,20)
+
+        ((5, 25), (10,15), x), # (5,9) (13,18) (16,25)
+        ((5,15), (10,15), x), # (5,9) (13,18)
+        ((10,20), (10,15), x), # (13,18) (16,20)
+
+        ((10,15), (5,20), x), # (13, 18)
+        ((10,15), (10,15), x), # (13,18)
+        ((10,15), (10,20), x), # (13,18)
+        ((10,15), (5,15), x) # (13,18)
+    ]
+
+    for triple in test_intervals:
+        print(combine_two_intervals_and_shift(triple[0], triple[1], triple[2]))
 
 def get_distance_for_shifting(source: tuple, destination: tuple) -> int:
     return destination[0] - source[0]
@@ -74,13 +113,16 @@ def combine_two_intervals_and_shift(first: tuple, second: tuple, shifting_distan
 # case 1: no overlapping tuple
 # case 2: overlap on "left" side of second tuple
 # case 3: overlap on "right" side of second tuple
-# case 4: first interval overlaps second completely -> this should cover the case when intervals match
-# case 5: first interval completely in second interval
+# case 4: first interval overlaps second completely 
+# case 5: first interval completely in second interval -> this should cover the case when intervals match
+
+# Mistake found: case 5 didnt cover the case when the start or end of the intervals matches
 
     START = 0
     END = 1
     # case 1
-    if first[START] < second[START] or first[START] > second[END]:
+    # Mistake found: first[END] was first[START]
+    if first[END] < second[START] or first[START] > second[END]:
         return [first] # this tuple can stay as it is
 
     # 
@@ -94,14 +136,14 @@ def combine_two_intervals_and_shift(first: tuple, second: tuple, shifting_distan
         right_tuple = (second[END] + 1, first[END]) # right tuple can stay as it is
         return [left_tuple, right_tuple]
     # case 4:
-    elif first[START] <= second[START] and first[END] >= second[END]:
+    elif (first[START] < second[START] and first[END] >= second[END]) or (first[START] <= second[START] and first[END] > second[END]):
         left_tuple = (first[START], second[START] - END) # left tuple can stay as it is
         middle_tuple = (second[START] + shifting_distance, second[END] + shifting_distance) # middle tuple has to be mapped
         right_tuple = (second[END] + 1, first[END]) # right tuple can stay as it is
         return [left_tuple, middle_tuple, right_tuple]
     
     # case 5:
-    elif first[START] > second[START] and first[END] < second[END]:
+    elif first[START] >= second[START] and first[END] <= second[END]:
         return [(first[START] + shifting_distance, first[END] + shifting_distance)] # this has to be mapped
     else:
         print(first, second, shifting_distance)
