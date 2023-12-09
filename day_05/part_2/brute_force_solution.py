3c101b0bc09e7c8f40b8d620cccacecb925166cb
Emil Merle <emilmerle8@gmail.com>
2023-12-09 11:57:18 +0100

    Add solution for day 09 part 1

--- day_05/part_2/brute_force_solution.py
+++ day_05/part_2/brute_force_solution.py
@@ -2,13 +2,15 @@ import sys
 
 
 filename = "./../input_data"
+START = 0
+END = 1
 
 
 def main():
     map_dict = {}
     with open(filename, "r") as f:
         seed_ranges = get_seeds(f.readline())
-        print(seed_ranges)
+        # print(seed_ranges)
         map_order = []
 
         for line in f:
@@ -16,69 +18,53 @@ def main():
             if len(line_list) < 2:
                 continue
             elif len(line_list) == 2:
-                # map into dict
-                map_dict[line_list[0]] = []
+                # maps into dict
+                map_dict[line_list[0]] = {}
                 map_order.append(line_list[0])
                 continue
             elif len(line_list) == 3:
-                # ranges into map in dict
-                shifting_distance = int(line_list[0]) - int(line_list[1])
+                # source interval with distance for shifting into dict
+                destination_range = (
+                    int(line_list[0]),
+                    int(line_list[0]) + int(line_list[2]),
+                )
                 source_range = (
                     int(line_list[1]),
-                    int(line_list[1]) + int(line_list[2]) - 1,
+                    int(line_list[1]) + int(line_list[2]),
                 )
-                map_dict[map_order[-1]].append((source_range, shifting_distance))
+                shift_distance = get_distance_for_shifting(
+                    source_range, destination_range
+                )
+                map_dict[map_order[-1]][source_range] = shift_distance
                 continue
             else:
                 exit("Edge Case found!!!")
-
         
-        # für jede Seed range:
-        #       für jeden Seed
-        #           currentvalue = seed
-        #           für jede Map:
-        #               für jede range in der map:
-        #                   ist der currentvalue in der range:
-        #                       ja: currentvalue mappen und nächste map
 
-        lowest_location = sys.maxsize
+        lowest_value = sys.maxsize
         for seed_range in seed_ranges:
-            print("Starting input seed range:", seed_range)
+            print("Starting seed range:", seed_range)
             for seed in range(seed_range[0], seed_range[1]):
+                if seed % 10_000_000 == 0:
+                    print("Seed:", seed)
                 current_value = seed
-                print("Current value:", current_value)
                 for map in map_order:
-                    for single_range in map_dict[map]:
-                        if single_range[0][0] <= current_value < single_range[0][1]:
-                            current_value = current_value + single_range[1]
-                            break
-                        
-                if current_value < lowest_location:
-                    lowest_location = current_value
-            print("Current lowest location:", lowest_location)
-        
-        print("Lowest location:", lowest_location)
+                    value_after_map = return_mapped_value(current_value, map_dict[map])
+                    current_value = value_after_map
+                if current_value < lowest_value:
+                    lowest_value = current_value
 
-        """ for seed in seeds:
-            # print(seed)
-            current_value = seed
-            for map in map_order:
-                counter = 0
-                for single_range in map_dict[map]["source_ranges"]:
-                    if single_range[0] <= current_value <= single_range[1]:
-                        distance = current_value - single_range[0]
-                        current_value = (
-                            map_dict[map]["destination_ranges"][counter][0] + distance
-                        )
-                        # Mistake found: did not break but continue loop first
-                        break
-                    else:
-                        current_value = current_value
-                    counter += 1
-            final_values.append(current_value) """
+        print(lowest_value)
 
-        # print(f"The lowest location number is {min(final_values)}.")
+            
+def return_mapped_value(number: int, map_mapping_ranges: dict) -> int:
+    for range, shifting_value in map_mapping_ranges.items():
+        if number >= range[START] and number < range[END]:
+            return number + shifting_value
+    return number
 
+def get_distance_for_shifting(source: tuple, destination: tuple) -> int:
+    return destination[0] - source[0]
 
 def get_seeds(seed_line: str) -> list:
     seed_list = seed_line[7:].split()
---
+++ day_09/part_1/solution.py
@@ -0,0 +1,40 @@
+filename = "./../input_data"
+
+
+def main():
+    with open(filename, "r") as f:
+        input_sequences = []
+        for line in f:
+            seq = [int(x) for x in line.split()]
+            input_sequences.append(seq)
+
+        final_sum = 0
+        for sequence in input_sequences:
+            next_value = analyze_sequence(sequence)
+            print(sequence, next_value)
+            final_sum += next_value
+
+        print(f"The final sum of all next values is {final_sum}.")
+
+
+def analyze_sequence(sequence: list):
+    sequences = []
+    current_sequence = sequence
+    while len([x for x in current_sequence if x != 0]):
+        next_sequence = []
+        sequences.append(current_sequence)
+
+        for index in range(1, len(current_sequence)):
+            next_value = current_sequence[index] - current_sequence[index - 1]
+            next_sequence.append(next_value)
+
+        current_sequence = next_sequence
+
+    for index in range(len(sequences) - 2, -1, -1):
+        sequences[index].append(sequences[index][-1] + sequences[index + 1][-1])
+
+    return sequences[0][-1]
+
+
+if __name__ == "__main__":
+    main()
